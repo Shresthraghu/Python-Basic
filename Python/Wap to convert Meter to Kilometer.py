7ab31ae278295d558e17563718071c437690d09c
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 12:23:35 +0530

    Add files via upload
    Write a program to covert Meter to Kilometer.

---
+++ Python/Wap to convert Meter to Kilometer.py
@@ -0,0 +1,5 @@
+#Write a program to convert Meter to Kilometer .
+#conversion from m to km
+met=int(input("enter value of meter:"))
+mete=((met)/1000);
+print("after conversion value of kilo meter is :",mete);
