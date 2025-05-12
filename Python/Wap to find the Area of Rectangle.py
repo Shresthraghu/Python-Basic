3639ae8338ae44a085afcc6e1afc19a02c904263
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:08:44 +0530

    Add files via upload
    Write a Program to find the Area of Rectangle.

---
+++ Python/Wap to find the Area of Rectangle.py
@@ -0,0 +1,6 @@
+#Write a program to find out the Area of Rectangle.
+# Area of Rectangle
+length=int(input("Enter the value of Length"))
+width=int(input("Enter the value of width"))
+area=length*width;
+print("The area of Rectangle is :",area)
