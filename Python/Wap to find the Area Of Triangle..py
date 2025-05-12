5f108bb2df160f51f594916bf4b75dc9cf3989bc
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:05:37 +0530

    Add files via upload
    Write a Program to find the Area of Triangle.

---
+++ Python/Wap to find the Area Of Triangle..py
@@ -0,0 +1,7 @@
+#Write aprogram to find out the Area of Triangle.
+# Area of Triangle
+base=int(input("Enter the value of base :"))
+height=int(input("Enter the value of height"))
+area=((base*height)/2)
+print("The area of triangle is",area,"cm")
+
