ed45bfc9b03cf9c656a308615444f65ca25e44ce
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 12:25:09 +0530

    Add files via upload
    Milimeter to Liter conversion .

---
+++ Python/Wap to convert Milimetr to Liter.py
@@ -0,0 +1,5 @@
+#write a program to convert Milimeter to Liter.
+#conversion from mm to l
+mm=int(input("enter value of milimeter:"))
+lit=((mm)/1000);
+print("after conversion value of literes is :",lit);
