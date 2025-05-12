fc90c2ede601f6465f19f10551e70b633274665a
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:06:22 +0530

    Add files via upload
    Write a Program to find the Area of Square.

---
+++ Python/wap to find the Area of Square.py
@@ -0,0 +1,5 @@
+#write a program to find the Area of Square.
+# Area of Square:-
+side=int(input("Enter the value of side of a square:"))
+area=(side*side)
+print("The area of square is :",area,"cm")
