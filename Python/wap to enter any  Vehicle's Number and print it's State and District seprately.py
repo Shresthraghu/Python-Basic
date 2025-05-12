01193ac095d67532d61bc1fd97b200fa86aa8e41
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:24:12 +0530

    Add files via upload
    Write a program to enter any Vehicle Number and Print it's State and District seprately.

---
+++ Python/wap to enter any  Vehicle's Number and print it's State and District seprately.py
@@ -0,0 +1,7 @@
+#write a program to enter any Vehicle Number and print its state and district seprately.
+# vehicles Details :-
+regisNum=(input("Enter your vehicle  Registration number:"))
+rN=regisNum[0:2]
+sN=regisNum[2:4]
+print(rN)
+print(sN)
