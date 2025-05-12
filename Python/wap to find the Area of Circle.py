a3196a2b075c920bcbc7ec674753a0c80a98aade
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:09:46 +0530

    Add files via upload
    Write a program to find the Area of Circle .

---
+++ Python/wap to find the Area of Circle.py
@@ -0,0 +1,5 @@
+#Write a program to find the Area of Circle :-
+# Area of Circle
+radius=int(input("Enter the value of radius :"))
+area= ((3.14*radius)*radius);
+print("The area of Circle is :",area)
