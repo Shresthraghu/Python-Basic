08583e51f12c0f977efe77a8c96c77b57d12b40e
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-12 15:25:45 +0530

    Add files via upload
    Write a program to enter any Landline Number and retrive its Number only without District code.

---
+++ Python/Wap to  enter any Landline Number and retrive it's number only without District code..py
@@ -0,0 +1,5 @@
+#Write a program to enter any Landline Number and retrive it's Number only without Distict code.
+#Landline Number
+landLine=input("Enter your Landline Number:")
+ln=landLine[4:]
+print(ln)
