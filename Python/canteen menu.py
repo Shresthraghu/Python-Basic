b4ec02b8eb60fe7a836f10e541ce2b838016578c
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-21 00:22:27 +0530

    Add files via upload
    Write a Program to find out menu of your canteen for a particular day there must be menu for each day .

---
+++ Python/canteen menu.py
@@ -0,0 +1,18 @@
+#canteen's daily menu: (28.04.25)
+day=input("Enter day for checking menu :")
+if(day=='monday'):
+      print("on monday we have chole bhaturey.")
+elif(day=='tuesday'):
+      print("on tuesday we have samose and kachori.")
+elif(day=='wednesday'):
+      print("on wednesday we have kofte and naan.")
+elif(day=='thursday'):
+      print("on thursday we have schezwan rice.")
+elif(day=='friday'):
+      print("on friday we have fried rice.")
+elif(day=='saturday'):
+      print("on saturday we have chicken kawwab")
+elif(day=='sunday'):
+      print("on sunday we have Butter panner and kulcha.")
+else:
+      print("Enter week days.")   
