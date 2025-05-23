a64c678c16c00fe3f1b3c8a32465bdd34a4cd86a
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-24 02:15:13 +0530

    Add files via upload
    Inserting Value in list with position syntax and example in list .

---
+++ Syntax and Examples/inst.py
@@ -0,0 +1,11 @@
+nm=['anu','renu','monu','sonu']
+print(nm)
+nam=input("enter name")
+nm.append(nam)
+print(nm)
+nam=input("enter name")
+pos=int(input("enter position"))
+nm.insert(pos,nam)
+print(nm)
+#nm[pos]="divya"
+print(nm)
