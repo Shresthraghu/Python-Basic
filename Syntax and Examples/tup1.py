51ba2c43c9374fa91c148a28ef897da7fe396b17
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-24 12:34:33 +0530

    Add files via upload
    Tuple syntax and example .

---
+++ Syntax and Examples/tup1.py
@@ -0,0 +1,13 @@
+nm=()
+print(nm)
+nm=('anu','renu','monu','sonu')
+print(nm)
+for i in nm:
+    print(i)
+l=len(nm)
+for i in range(l):
+    print(i,nm[i])
+pos=nm.index('renu')
+print(pos)
+nm=list(nm)
+print(nm)
