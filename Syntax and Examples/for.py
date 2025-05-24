3c645aeae49f3a7c6475aa49f440e5b46a65a0a6
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-05-24 12:30:33 +0530

    Add files via upload
    For loop syntax with example .

---
+++ Syntax and Examples/for.py
@@ -0,0 +1,21 @@
+nm="Prema"
+for i in nm:
+    print(i)
+print("-------------------")
+for i in range(0,5,1):
+    print(i)
+print("-------------------")
+for i in range(5):
+    print(i)
+print("-------------------")
+n=5
+for i in range(1,11):
+    t=n*i
+    print(n,"*",i,"=",t)
+print("-------------------")
+for i in range(5,-1,-1):
+    print(i)
+print("-------------------")
+l=len(nm)
+for i in range(l):
+    print(i,nm[i])
