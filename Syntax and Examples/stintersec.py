125fbda1dd12314e8496b44ddacb4e8ae0129695
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-02 21:45:04 +0530

    Add files via upload
    Printing common element from both the sets by making a new set and putting common value in it.
    
    syntax : set1.intersection_update(set2)

---
+++ Syntax and Examples/stintersec.py
@@ -0,0 +1,13 @@
+# Printing common element from both the sets by making a new set .
+
+# syntax : set1.intersection_update(set2)
+nm1=['Anu','Renu','Monu','Sonu']
+nm2=['Chiya','Anu','Pooja','Lata']
+nm1=set(nm1)
+nm2=set(nm2)
+print(nm1)
+print(nm2)
+nm3=nm1.intersection(nm2)
+print(nm1)
+print(nm2)
+print(nm3)
