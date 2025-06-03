3996f381b5c9d6a767f2244fd9e0416e6aea6e0d
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-04 01:29:21 +0530

    Add files via upload
     Checking specific character exists in a string or not .

---
+++ Nested If conditions/checking specific character in a string.py
@@ -0,0 +1,12 @@
+# Write  a program to check if the specific character exists in a string or not and print the appropriate message.
+
+# checking specific character in a string :-
+
+name=input("Enter the String : ")
+char=input("Enter the specific character : ")
+
+if char in name :
+    print("Specific character exists in a string.")
+
+else:
+  print("Specific character does not exists in a string.")  
