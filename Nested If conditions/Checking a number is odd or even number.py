ad4efe551946e4a0ed77ac24d022081addbef127
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-04 01:36:45 +0530

    Add files via upload
     write a program to check if a given number is even or odd number and if the number is even print even number , else print odd number .

---
+++ Nested If conditions/Checking a number is odd or even number.py
@@ -0,0 +1,11 @@
+# write a program to check if a given number is even or odd number and if the number is even print even number , else print odd number.
+
+# Checking a number is odd or even number.
+
+num=int(input("Enter any number : "))
+
+if (num/2):
+    print(num,"is a even number.")
+
+else :
+    print(num,"is a odd number.")
