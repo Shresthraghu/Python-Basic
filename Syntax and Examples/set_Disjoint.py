06493c0e3c6103363c25b8dfc58252011c104cfc
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-04 00:04:27 +0530

    Add files via upload
    Disjoint syntax and Example.
    
    nm1.isdisjoint(nm2)

---
+++ Syntax and Examples/set_Disjoint.py
@@ -0,0 +1,12 @@
+# Disjoint syntax and Example.
+
+# Syntax : nm1.isdisjoint(nm2)
+
+nm1=['anu','renu','monu','sonu','divya']
+nm2=['pooja','lata','jeetu','sonu']
+nm1=set(nm1)
+nm2=set(nm2)
+print(nm1)
+print(nm2)
+an=nm1.isdisjoint(nm2)
+print(an)
