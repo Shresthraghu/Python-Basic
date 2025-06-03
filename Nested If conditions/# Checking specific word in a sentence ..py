6fd99c1fd3c2508807471c0fa9eda5949c8e44fb
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-04 01:30:47 +0530

    Add files via upload
    Write a program for checking specific word exist in a sentence or not .

---
+++ Nested If conditions/# Checking specific word in a sentence ..py
@@ -0,0 +1,12 @@
+# Write  a program to check if the specific word exists in a sentence or not and print the appropriate message.
+
+# Checking specific word in a sentence :-
+
+stn=input("Enter sentence : ")
+word=input("Enter the word to be searched in a sentenced :")
+
+if word in stn :
+    print("specific word exists in a sentence.")
+
+else:
+    print("specific word does not exists in a sentence.")
