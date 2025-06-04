6a155fb0996f48d3b8512b0d6a4113c886cf6dd0
Shresthraghu <134433890+Shresthraghu@users.noreply.github.com>
2025-06-04 22:14:27 +0530

    Add files via upload
    Function syntax and example .

---
+++ Syntax and Examples/fun1.py
@@ -0,0 +1,20 @@
+def sum():
+    a=int(input("enter first no"))
+    b=int(input("enter second no"))
+    c=a+b
+    print("sum is",c)
+
+def prod():
+    a=int(input("enter first no"))
+    b=int(input("enter second no"))
+    c=a*b
+    print("prod is",c)
+
+print("function calling first time")
+sum()
+prod()
+print("function calling second time")
+prod()
+sum()
+
+
